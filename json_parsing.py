4360580705c6abc8d39264c4178bcd601d8f3fa2
Пичурин Ростислав Владиславович <r.pichurin@voskhod.ru>
2021-09-16 02:08:46 +0300

    HomeWork/Ex5: Парсинг JSON

---
+++ json_parsing.py
@@ -0,0 +1,12 @@
+import json
+
+json_text = '{' \
+                '"messages":[' \
+                    '{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
+                    '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}' \
+                ']' \
+            '}'
+
+json = json.loads(json_text)
+print(json["messages"][1]["message"])
+
